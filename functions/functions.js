f7305e006f7ebde7fe7fa01ea7b98a03d4ad3043
chuma-beep <wisboynelson123@gmail.com>
2024-01-03 04:33:20 +0100

    repo to track js learning

---
+++ functions/functions.js
@@ -0,0 +1,17 @@
+//function that counts through an array.
+
+let ageOfDogs = [2, 3, 5, 11, 10, 4];
+
+function count(para){
+  let i = []
+  for(let i = 0; i != ageOfDogs.length; i++){
+    return i.push();
+  
+
+
+
+    console.log(para)
+  }
+}
+  
+  console.log(count(ageOfDogs));
