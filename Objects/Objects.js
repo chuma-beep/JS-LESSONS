5522536fa8d8f5c866c565dcad14af59a5cd901b
Chukwuma Wisdom Anwaegbu <wisboynelson123@gmail.com>
2024-01-10 15:52:52 +0100

    finished

--- Objects/Objects.js
+++ Objects/Objects.js
@@ -114,33 +114,37 @@ class Group{
     //add value 
     add(value){
       if(!this.has(value)){
-        this.members.push(value);
+        this.members.push(value)
       }
     }
 
     //deletes value
     delete(value){
-        this.members = this.filter(v => v !== value);
+        this.members = this.filter(v => v !== value)
     }
 
     has(value){
-     this.members.includes(value);
+     this.members.includes(value)
     }
-
+   //static
     static from(collection){
-        let group
+        let group = new Group
+        for (let value of collection){
+            group.add(value)
+        }
+        return group
     }
 
 
 }
 
 
-let group = Group.from([10, 20]);
-console.log(group.has(10));
+let group = Group.from([10, 20])
+console.log(group.has(10))
 // → true
-console.log(group.has(30));
+console.log(group.has(30))
 // → false
-group.add(10);
-group.delete(10);
-console.log(group.has(10));
+group.add(10)
+group.delete(10)
+console.log(group.has(10))
 // → false
