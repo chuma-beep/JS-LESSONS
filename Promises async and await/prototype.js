57f6fe1dcd28777d9401c606df946d84d44201ab
chuma-beep <wisboynelson123@gmail.com>
2024-02-23 14:22:29 +0100

    new update

---
+++ Promises async and await/prototype.js
@@ -0,0 +1,12 @@
+//prototype manipulation
+//the code below is not advisable but it's possible
+String.prototype.altSchool = function(){
+    return this.toUpperCase()
+}
+let name = 'wisdom' //literal 1 - use constructor to construct a new varaile, 
+                    //2- converts the constructor object to a primitive data type
+console.log(typeof name)
+
+console.log(name.altSchool()) 
+
+//constructors
