f07c32678a882bb9f9796d10aa409d7bad7b0c01
chuma-beep <wisboynelson123@gmail.com>
2024-03-06 08:41:11 +0100

    worked with nodeList and nodechild etc

--- new-project/main.js
+++ new-project/main.js
@@ -1,9 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import javascriptLogo from './javascript.svg'
+import './style.css'
+import '/bling.js' 
+import viteLogo from '/vite.svg'
 
-document.querySelector('#app').innerHTML = `
+
+$('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -14,11 +16,59 @@ document.querySelector('#app').innerHTML = `
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
-    </div>
+    </div
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
+    <table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Instrument</th>
+    </tr>
+  </thead>
+  <tfoot>
+    <tr>
+      <th>Name</th>
+      <th>Instrument</th>
+    </tr>
+  </tfoot>
+  <tbody>
+    <tr>
+      <td>John Lennon</td>
+      <td>Rhythm Guitar</td>
+    </tr>
+    <tr>
+      <td>Paul McCartney</td>
+      <td>Bass</td>
+    </tr>
+    <tr>
+      <td>George Harrison</td>
+      <td>Lead Guitar</td>
+    </tr>
+    <tr>
+      <td>Ringo Starr</td>
+      <td>Drums</td>
+    </tr>
+  </tbody>
+</table>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+setupCounter($('#counter'))
+
+
+//list of ChildNodes
+
+//childNodes - firstChild, lastChild
+
+//children -firstElementchild
+
+//parentNode 
+
+
+const table = $('table')
+const row = table.rows
+console.log(table.row)
+
+
