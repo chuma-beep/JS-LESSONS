d4307264d6deee72a498f55b6fa91d2655d8377f
chuma-beep <wisboynelson123@gmail.com>
2024-03-07 08:42:42 +0100

    lessons-revision

--- new-project/bling.js
+++ new-project/bling.js
@@ -24,3 +24,46 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
     elem.on(name, fn);
   });
 }
+
+//Node properties, Attribute and Properties, 
+//Modifying the documnet, styles and Classes
+
+//select a particular content to discover the inner workings of the DOM
+
+let content = document.body.firstChild.nodeValue;
+console.log(content)
+
+
+
+
+
+//attribute
+
+
+//classes
+//classList(), className, attribute(CCRUD)
+
+// styles
+
+//document.createElement('element') - this creates a new element
+ 
+//insertion methods - appendChild, insertBefore, insertAdjacentElement, insertAdjacentHTML, insertAdjacentText
+// apppend - adds a new element to the end of the list of children of a specified parent node
+//prepend - adds a new element to the beginning of the list of children of a specified parent node
+//before - inserts a new element before a specified element
+//replaceWith - replaces the specified element with a new element
+//after - inserts a new element after a specified element
+//newDiv
+//allow us to insert new elements into the DOM
+// allow us to inseert(text, element, comment)
+// div.before(newDiv)
+
+// div.prepend(newDiv)
+// div.append(newDiv)
+// div.after(newDiv)
+
+
+//what is data hypen? 
+//what is a unique way to really have your own attribute?
+//data-hypen?
+
--- new-project/main.js
+++ new-project/main.js
@@ -72,3 +72,46 @@ const row = table.rows
 console.log(table.row)
 
 
+const newDiv = document.createElement('div')
+
+
+//textContent, innerHTML
+newDiv.textContent = 'Modifying the DOM';
+
+app.append(newDiv)
+
+
+
+
+
+//attribute
+
+
+//classes
+//classList(), className, attribute(CCRUD)
+
+// styles
+
+//document.createElement('element') - this creates a new element
+ 
+//insertion methods - appendChild, insertBefore, insertAdjacentElement, insertAdjacentHTML, insertAdjacentText
+// apppend - adds a new element to the end of the list of children of a specified parent node
+//prepend - adds a new element to the beginning of the list of children of a specified parent node
+//before - inserts a new element before a specified element
+//replaceWith - replaces the specified element with a new element
+//after - inserts a new element after a specified element
+//newDiv
+//allow us to insert new elements into the DOM
+// allow us to inseert(text, element, comment)
+// div.before(newDiv)
+
+// div.prepend(newDiv)
+// div.append(newDiv)
+// div.after(newDiv)
+
+
+//what is data hypen? 
+//what is a unique way to really have your own attribute?
+//data-hypen?
+
+//insertAdjacentElement
